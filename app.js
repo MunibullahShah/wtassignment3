d7c492ab0d23dc2e4196026772fe4633f47e9035
Munibullah Shah <49975985+MunibullahShah@users.noreply.github.com>
2021-06-16 19:55:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,80 +1,86 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+var cors = require('cors');
+
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-var arr=[
-    {
-      "name": "irfan",
-      "email": "afsajj@gmac.com",
-      "phone_number": ["3232","32324423"],
-      "gender": "male",
-      "course_code": "MT323",
-      "address": {
-        "country": "pakistan",
-        "city": "lahore",
-        "street_address": "no known"
-      },
-      "id":"0"
+const jsonObject = [
+
+
+  {
+    "name": "irfan",
+    "email": "afsajj@gmac.com",
+    "phone_number": ["3232", "32324423"],
+    "gender": "male",
+    "course_code": "MT323",
+    "address": {
+      "country": "pakistan",
+      "city": "lahore",
+      "street_address": "not known"
     },
-  ];
+    "id": "0"
+  },
+];
 
-app.get('/',(req,res)=>{
-  res.json({msg:"success",arr})
-})
-
-app.post('/',(req,res)=>{
-  const {id,name,email,gender,address,course_code,phone_number}=req.body;
-    const {street_address,city,country}=address;
-  const newFaculty={
-    id,
-    name,
-    email,
-    gender,
-    address:{
-      street_address,
-      city,
-      country
-    },
-    course_code,
-    phone_number
-  }
-  if(id){
-    arr.push(newFaculty)
-    return res.json({
-        success: "success",
-        arr,
-      });
-  }else {
-    return res.json({
-      msg:"provide id feild for record"
-    })
-  }
-})
-
-app.put('/:id',(req,res)=>{
-  i = arr.findIndex((obj => obj.id == req.params.id));
-  const result=arr[i];
-
-  var merge={...result,...req.body};
-  arr[i]=merge;
-
-  res.status(200).json({
-    status: 'success',
-    arr,
-  });
-})
-
-app.delete('/:id',(req,res)=>{
-  i = arr.findIndex((obj => obj.id == req.params.id));
-  arr.splice(i,1);
-  res.status(204).json({
-    status: 'deleted',
-  });
-})
+let startID = jsonObject.length;
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Running at ${PORT}`);
+app.get("/faculty", function (req, res) {
+  return res.json(jsonObject);
 });
+
+app.post("/faculty", function (req, res) {
+  console.log();
+  jsonObject.push(req.body);
+  jsonObject[jsonObject.length - 1].id = startID++;
+  res.send(jsonObject);
+});
+
+app.get("/faculty/:index", function (req, res) {
+  for (let index = 0; index < jsonObject.length; index++) {
+    if (jsonObject[index].id == req.params.index)
+      return res.json(jsonObject[index]);
+  }
+  return res.status(404).send("Record not found");
+});
+
+app.put("/faculty/:index", function (req, res) {
+  for (let index = 0; index < jsonObject.length; index++) {
+    if (jsonObject[index].id == req.params.index) {
+      var keys = Object.keys(req.body);
+      for (let key = 0; key < keys.length; key++) {
+        jsonObject[index][keys[key]] = req.body[keys[key]];
+      }
+      return res.json(jsonObject[index]);
+    }
+  }
+  return res.status(404).send("Failed to update, record not found.");
+});
+
+app.delete("/faculty/:index", function (req, res) {
+  for (let index = 0; index < jsonObject.length; index++) {
+    if (jsonObject[index].id == req.params.index) {
+      jsonObject.splice(index, 1);
+      return res.status(200).send("Removed!");
+    }
+  }
+  return res.status(200).send("Removed.");
+});
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+const PORT = process.env.PORT || 2500;
+
+app.listen(PORT, () => {
+  console.log(`running at PORT ${PORT}`);
+});
+
+
